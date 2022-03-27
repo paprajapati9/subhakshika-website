593d4e438781521c98a01ede6eb9ad97f4c066ce
Aryan Sethi <aryan.rdps@gmail.com>
2022-03-27 17:36:55 +0530

    Added SwiperJS API and integrated to our projects section

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,25 +1,49 @@
 // Logic to change Picture
 
 const circlesContainer = document.querySelector(".circles");
-const circle1 = document.querySelector(".circle1"); 
-const circle2 = document.querySelector(".circle2"); 
-const circle3 = document.querySelector(".circle3"); 
-
-
+const circle1 = document.querySelector(".circle1");
+const circle2 = document.querySelector(".circle2");
+const circle3 = document.querySelector(".circle3");
 
 // Event to change colors
-circle1.addEventListener("click" , e => {
+circle1.addEventListener("click", e => {
     circle2.classList.remove("current");
     circle3.classList.remove("current");
     circle1.classList.add("current");
 });
-circle2.addEventListener("click" , e => {
+circle2.addEventListener("click", e => {
     circle1.classList.remove("current");
     circle3.classList.remove("current");
     circle2.classList.add("current");
 });
-circle3.addEventListener("click" , e => {
+circle3.addEventListener("click", e => {
     circle1.classList.remove("current");
     circle2.classList.remove("current");
     circle3.classList.add("current");
+});
+
+// Our Projects swiper
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        bulletActiveClass: "swiper-pagination-bullet-active our-projects__button--active",
+        bulletClass: "swiper-pagination-bullet our-projects__button"
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar"
+    }
 });
